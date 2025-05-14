1346ac4104ca0d026a6caac1ddffe9a069c01eae
Yuri <142381932+Yurei04@users.noreply.github.com>
2025-05-14 19:17:38 +0800

    initial(contents): foundational code

---
+++ hackunited/src/pages/layout/blog-page.jsx
@@ -0,0 +1,6 @@
+export default function BlogPageLayout () {
+    return (
+        <>
+        </>
+    )
+}
---
+++ hackunited/src/pages/layout/team-page.jsx
@@ -0,0 +1,6 @@
+export default function TeamPageLayout () {
+    return (
+        <>
+        </>
+    )
+}
