b56bf74e64be8d5fb4c585bcd504dda56c73b440
Yuri <142381932+Yurei04@users.noreply.github.com>
2025-05-19 19:51:30 +0800

    removal(imports)

--- hackunited/src/app/home-page.jsx
+++ hackunited/src/app/home-page.jsx
@@ -1,13 +1,11 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import Hero from "@/pages/hero";
-import SiteHeader from "@/app/site-header";
 import About from "@/pages/about";
 import Community from "@/pages/community";
 import Events from "@/pages/events";
 import Goals from "@/pages/goals";
 import Initiatives from "@/pages/initiatives";
 import JoinUs from "@/pages/join-us";
-import ContactPage from "@/pages/contact-us";
 import Footer from "@/pages/footer";
 import NavBar from "@/app/navbar";
 
